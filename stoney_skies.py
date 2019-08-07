d947b59101131a66ede533b86ba576a289d729dc
Syrus_Surface <44655931+SyrusDrake@users.noreply.github.com>
2019-08-07 14:08:03 +0100

    Markers can be placed by the user

--- stoney_skies.py
+++ stoney_skies.py
@@ -2,24 +2,39 @@
 # Stoney Skies Alpha
 # This version is non-functional and used for the design and development of
 # the GUI.
-# Last change: 06.08.2019
+# Last change: 07.08.2019
 
 import tkinter as tk
+
+marker_ID = 1
 
 # <cf> Classes
 
 
 class Marker:
-    def __init__(self, canvas, xc, yc, color):
+    def __init__(self, canvas, xc, yc):
         self.radius = 7
         self.x1 = xc - self.radius
         self.y1 = yc - self.radius
         self.x2 = xc + self.radius
         self.y2 = yc + self.radius
-        self.id = canvas.create_oval(self.x1, self.y1, self.x2, self.y2, fill=color)
+        self.id = canvas.create_oval(self.x1, self.y1, self.x2, self.y2, fill='blue')
+
+
 
 # </cf> Classes
 
+# <cf> Functions
+
+def place_marker(event):
+    global marker_ID
+    print(f"Blue has ID {marker_ID}")
+    Marker(canvas, event.x, event.y)
+    marker_ID += 1
+    print(f"Next will have marker ID {marker_ID}")
+    # print ("clicked at", event.x, event.y)
+
+# </cf> Functions
 
 # <cf> drawing the interface
 
@@ -32,6 +47,7 @@ control = tk.Frame(bg='green')
 control.place(relheight=0.9, relwidth=0.18, rely=0.05, relx=0.01)
 
 canvas = tk.Canvas(cursor='crosshair', bd=5, relief='groove')
+canvas.bind("<Button-1>", place_marker)
 canvas.place(relheight=0.90, relwidth=0.75, rely=0.05, relx=0.2)
 
 b_undo = tk.Button(control, text="Undo", font=30)
@@ -43,10 +59,10 @@ b_redo.pack(pady=5, fill='x')
 b_clear = tk.Button(control, text="Clear canvas", font=30)
 b_clear.pack(pady=5, fill='x')
 
-# </cf> drawing the interface
+b_clear = tk.Button(control, text="Test", font=30)
+b_clear.pack(pady=5, fill='x')
 
-marker1 = Marker(canvas, 200, 200, 'green')
-marker2 = Marker(canvas, 300, 150, 'blue')
+# </cf> drawing the interface
 
 root.geometry(f'{root_width}x{root_height}+100+100')
 root.mainloop()
