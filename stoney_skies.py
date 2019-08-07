5ab94dd346b6a590c982b27f19c1b6b3881ce20c
Syrus_Surface <44655931+SyrusDrake@users.noreply.github.com>
2019-08-07 15:49:57 +0100

    Added undo function

--- stoney_skies.py
+++ stoney_skies.py
@@ -6,19 +6,26 @@
 
 import tkinter as tk
 
-marker_ID = 1
+marker_ID = 0
+list_of_markers = []
 
 # <cf> Classes
 
 
 class Marker:
-    def __init__(self, canvas, xc, yc):
+    def __init__(self, canvas, xc, yc, color):
         self.radius = 7
+        self.xc = xc
+        self.yc = yc
         self.x1 = xc - self.radius
         self.y1 = yc - self.radius
         self.x2 = xc + self.radius
         self.y2 = yc + self.radius
-        self.id = canvas.create_oval(self.x1, self.y1, self.x2, self.y2, fill='blue')
+        self.id = canvas.create_oval(self.x1, self.y1, self.x2, self.y2, fill=color)
+
+    def return_coordinates(self):
+        coordinates = [self.xc, self.yc]
+        return(coordinates)
 
 # </cf> Classes
 
@@ -27,12 +34,22 @@ class Marker:
 
 def place_marker(event):
     global marker_ID
-    Marker(canvas, event.x, event.y)
+    global list_of_markers
+    list_of_markers.append(Marker(canvas, event.x, event.y, 'red'))
     marker_ID += 1
 
 
 def clear_canvas():
     canvas.delete('all')
+
+
+def undo():
+    canvas.delete(list_of_markers[-1].id)
+    del list_of_markers[-1]
+
+def test():
+    pass
+
 
 # </cf> Functions
 
@@ -51,7 +68,7 @@ canvas = tk.Canvas(cursor='crosshair', bd=5, relief='groove')
 canvas.bind("<Button-1>", place_marker)
 canvas.place(relheight=0.90, relwidth=0.75, rely=0.05, relx=0.2)
 
-b_undo = tk.Button(control, text="Undo", font=30)
+b_undo = tk.Button(control, text="Undo", font=30, command=undo)
 b_undo.pack(pady=5, fill='x')
 
 b_redo = tk.Button(control, text="Redo", font=30)
@@ -60,10 +77,14 @@ b_redo.pack(pady=5, fill='x')
 b_clear = tk.Button(control, text="Clear canvas", font=30, command=clear_canvas)
 b_clear.pack(pady=5, fill='x')
 
-b_clear = tk.Button(control, text="Test", font=30)
-b_clear.pack(pady=5, fill='x')
+b_test = tk.Button(control, text="Test", font=30, command=test)
+b_test.pack(pady=5, fill='x')
+
+b_quit = tk.Button(control, text="Quit", font=30, command=root.quit)
+b_quit.pack(pady=5, fill='x')
 
 # </cf> drawing the interface
 
+testMarker = canvas.create_oval(50, 50, 100, 100, fill='yellow')
 root.geometry(f'{root_width}x{root_height}+100+100')
 root.mainloop()
