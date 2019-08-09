b434d6a76c65154d37cc2ce160ef8bd2389f6f32
Syrus_Surface <44655931+SyrusDrake@users.noreply.github.com>
2019-08-09 13:47:54 +0100

    Remove markers with right-click. Undo function deactivated.

--- stoney_skies.py
+++ stoney_skies.py
@@ -2,12 +2,12 @@
 # Stoney Skies Alpha
 # This version is non-functional and used for the design and development of
 # the GUI.
-# Last change: 07.08.2019
+# Last change: 09.08.2019
 
 import tkinter as tk
 
-marker_ID = 0   # For assigning IDs to the markers. Probably unnecessary.
-list_of_markers = []    # The "collection" of all markers.
+marker_ID = 1   # For assigning IDs to the markers. Necessary becausue canvas object count starts at
+marker_dictionary = {}    # The "collection" of all markers.
 
 # <cf> Classes
 
@@ -37,11 +37,11 @@ class Marker:
 
 
 def place_marker(event):
-    # Funuction to add the marker. Adds it to list_of_markers. marker_ID
-    # probably unnecessary.
+    # Funuction to add the marker. Adds it to marker_dictionary.
     global marker_ID
-    global list_of_markers
-    list_of_markers.append(Marker(canvas, event.x, event.y, 'red'))
+    global marker_dictionary
+    marker_dictionary[marker_ID] = (Marker(canvas, event.x, event.y, 'red'))
+    print(marker_dictionary)
     marker_ID += 1
 
 
@@ -50,11 +50,19 @@ def clear_canvas():
     canvas.delete('all')
 
 
-def undo():
+# def undo():
     # Undo function deletes the marker itself as well as its object held
-    # in the list_of_markers.
-    canvas.delete(list_of_markers[-1].id)
-    del list_of_markers[-1]
+    # in the marker_dictionary.
+    # canvas.delete(marker_dictionary[-1].id)
+    # del marker_dictionary[-1]
+
+
+def delete_marker(event):
+    # Function to delete the current marker under the mouse pointer.
+    object_id = (canvas.find_withtag('current')[0])  # object ID under cursor
+    canvas.delete(object_id)  # deletes the object under cursor
+    del marker_dictionary[object_id]  # deletes the relevant marker object
+
 
 def test():
     # Function for testing function. Gets called by button of same name.
@@ -73,12 +81,12 @@ root = tk.Tk()
 
 control = tk.Frame(bg='green')
 control.place(relheight=0.9, relwidth=0.18, rely=0.05, relx=0.01)
-
 canvas = tk.Canvas(cursor='crosshair', bd=5, relief='groove')
-canvas.bind("<Button-1>", place_marker)
+canvas.bind('<Button-1>', place_marker)
+canvas.bind('<Button-3>', delete_marker)
 canvas.place(relheight=0.90, relwidth=0.75, rely=0.05, relx=0.2)
 
-b_undo = tk.Button(control, text="Undo", font=30, command=undo)
+b_undo = tk.Button(control, text="Undo", font=30)
 b_undo.pack(pady=5, fill='x')
 
 b_redo = tk.Button(control, text="Redo", font=30)
@@ -95,6 +103,5 @@ b_quit.pack(pady=5, fill='x')
 
 # </cf> drawing the interface
 
-testMarker = canvas.create_oval(50, 50, 100, 100, fill='yellow')
 root.geometry(f'{root_width}x{root_height}+100+100')
 root.mainloop()
