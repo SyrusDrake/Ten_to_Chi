001f3e14c0645b16ec2628a8ad5270b0b8d72be6
SyrusDrake <flo.fruehwirth@gmail.com>
2020-04-21 00:26:37 +0200

    Marker angles
    Calculates angles between markers

--- astrolabe.py
+++ astrolabe.py
@@ -10,36 +10,60 @@ import shelve
 # random
 # marker_list = [{'ID': 1, 'x': 180, 'y': 538}, {'ID': 2, 'x': 223, 'y': 542}, {'ID': 3, 'x': 160, 'y': 48}, {'ID': 4, 'x': 182, 'y': 50}, {'ID': 5, 'x': 189, 'y': 223}]
 # check
-marker_list = [{'ID': 1, 'x': 261, 'y': 207}, {'ID': 2, 'x': 329, 'y': 340}, {'ID': 3, 'x': 441, 'y': 343}, {'ID': 4, 'x': 497, 'y': 476}, {'ID': 5, 'x': 626, 'y': 397}]
+marker_list = {'1': {'x': 261, 'y': 207}, '2': {'x': 329, 'y': 340}, '3': {'x': 441, 'y': 343}, '4': {'x': 497, 'y': 476}, '5': {'x': 626, 'y': 397}}
 
 star_distances = shelve.open('star_save_test')['distances']
 normalized_star_distances = {}
 dis_dif = {}
 dis_dif_comb = {}
 marker_num = len(marker_list)
+print(star_distances)
 
 
 # Calculates distances from a master marker to all other points
-def calculate_marker_distances(*markers):
+def calculate_marker_distances(**markers):
     distances = {}
-    master = markers[0]
-    x1 = master['x']
-    y1 = master['y']
-    for marker in marker_list[1:]:  # Goes through all markers except the first
-        x2 = marker['x']
-        y2 = marker['y']
+    x1 = markers[list(markers.keys())[0]]['x']
+    y1 = markers[list(markers.keys())[0]]['y']
+    for point in markers:  # Goes through all markers except the first
+        x2 = markers[point]['x']
+        y2 = markers[point]['y']
         dis = m.sqrt((x2-x1)**2+(y2-y1)**2)  # calculates the distances
-        distances[f"mdis_{marker['ID']}"] = dis  # adds the result to the list of distances
+        if dis == 0:
+            pass
+        else:
+            distances[point] = dis
     distances = {k: v for k, v in sorted(distances.items(), key=lambda item: item[1])}  # sorts the dictionary entries by value
     return distances
 
+# Calculates angles between Star1-Star2 and Star1-StarX
+def calculate_marker_angles(**markers):
+    angles = {}
+    # Extraxts the x and y values of the first two points in the list
+    m1 = markers[list(markers.keys())[0]]['x']
+    m2 = markers[list(markers.keys())[0]]['y']
+    s1 = markers[list(markers.keys())[1]]['x']
+    s2 = markers[list(markers.keys())[1]]['y']
+    d = m.atan2(s2-m2, s1-m1) # Saves the first part of the equation as a constant so it doesn't have to be repeated
+
+    # Repeats the calculations for all points in the list except the first two
+    for i in range(2, len(markers)):
+        x1 = markers[list(markers.keys())[i]]['x']
+        x2 = markers[list(markers.keys())[i]]['y']
+        ang = m.degrees(d - m.atan2(x2-m2, x1-m1))
+        # Adds 360 to negative values to only return positive angles
+        if ang < 0:
+            ang += 360
+        angles[list(markers.keys())[i]] = ang
+    return angles
+
 
 # Normalizes all distances so that one is 1.0 and all others are fractions of that
-def normalize(**distances):
+def normalize(**values):
     normalized_distances = {}
-    ref_distance = list(distances.values())[0]  # Takes the first entry in the list of distances
-    for i in distances:  # Iterates through the entire list and divides all distances by the chosen reference distance.
-        normalized_distances[i] = (distances[i]/ref_distance)
+    ref_distance = list(values.values())[0]  # Takes the first entry in the list of distances
+    for i in values:  # Iterates through the entire list and divides all distances by the chosen reference distance.
+        normalized_distances[i] = (values[i]/ref_distance)
     return normalized_distances  # returns a new list of normalized distances
 
 
@@ -48,8 +72,11 @@ for i in star_distances:
     normalized_star_distances[i] = normalize(**star_distances[i])
 
 
-marker_distances = calculate_marker_distances(*marker_list)
+marker_distances = calculate_marker_distances(**marker_list)
 normalized_marker_distances = normalize(**marker_distances)
+marker_angles = calculate_marker_angles(**marker_list)
+normalized_marker_angles = normalize(**marker_angles)
+
 
 # Compares normalized marker and star distances
 for d in normalized_star_distances:
@@ -69,6 +96,7 @@ dis_dif_comb = {k: v for k, v in sorted(dis_dif_comb.items(), key=lambda item: a
 
 # Prints the HIP ID of the star with the least deviation.
 print(list(dis_dif_comb.keys())[0])
+
 
 # Analytics code to compare expected and actual results
 # x = list(dis_dif_comb.keys())[0]
