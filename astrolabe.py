dbffb31b965fea1bf055c9a7a65a8e7be76f5d8e
SyrusDrake <flo.fruehwirth@gmail.com>
2020-09-10 01:26:05 +0200

    Improved Astrolabe
    Improved hit-count and cleaned up code

--- astrolabe.py
+++ astrolabe.py
@@ -5,14 +5,21 @@ from pathlib import Path
 ybp = 0
 matches = {}
 norma_markers = {}
+debug = open("debug.txt", "w+")
 
 
-filename = Path.cwd() / "Test_Cassiopeia" / "test.smp"
+# filename = Path.cwd() / "Test_Cassiopeia" / "test.smp"
+filename = Path.cwd() / "Test_Cassiopeia" / "full.smp"
 star_distances = shelve.open(str(filename))['distances']
 star_angles = shelve.open(str(filename))['angles']
+normd_star = shelve.open(str(filename))['normalized_distances']
+norma_star = shelve.open(str(filename))['normalized_angles']
 
-filename = Path.cwd() / "cassiopeia.ptn"
+filename = Path.cwd() / "stier.ptn"
+# filename = Path.cwd() / "cassiopeia.ptn"
 markers = shelve.open(str(filename))['marker_list']
+
+print(star_distances['26451']['15900'])
 
 
 def calculate_marker_distances(**markers):
@@ -57,7 +64,6 @@ def calculate_marker_bearings(**markers):
 marker_bearings = calculate_marker_bearings(**markers)
 
 marker_distances = calculate_marker_distances(**markers)
-print(marker_distances)
 
 normd_markers = normalize(**marker_distances)
 
@@ -70,42 +76,23 @@ del marker_bearings[list(normd_markers)[0]]  # removes "normalizer" from bearing
 for i in marker_bearings:
     b2 = marker_bearings[i]
     if b1 >= b2:
-        ang = b1 - b2
-    else:
         ang = b2 - b1
+    else:
+        ang = b1 - b2
     temp_dict[i] = ang
 
 # Sorts the normalized angles not on their values but on the values of the distances
 for k in list(normd_markers.keys()):
     norma_markers[k] = temp_dict[k]
 
-normd_star = {}
-
-for i in star_distances:
-    normd_star[i] = {}
-    for d in star_distances[i]:
-        normd_star[i][d] = {}
-        divisor = star_distances[i][d]
-        for x in star_distances[i]:
-            normalized = star_distances[i][x]/divisor
-            normd_star[i][d][x] = normalized
-
-norma_star = {}
-
-for i in star_angles:
-    norma_star[i] = {}
-    for s in star_angles[i]:
-        norma_star[i][s] = {}
-        minuend = star_angles[i][s]
-        for x in star_angles[i]:
-            normalized = minuend-star_angles[i][x]
-            norma_star[i][s][x] = normalized
-
 
 for a in normd_star:
-    matches[f'{a}'] = {}
-    matches[f'{a}']['Matches'] = 0
+    # matches[f'{a}'] = {}
+    # matches[f'{a}']['Matches'] = 0
     for n in normd_star[a]:
+        mid = f'{a}-{n}'
+        matches[mid] = {}
+        matches[mid]['Matches'] = 0
         for t in normd_star[a][n]:
             compd_star = normd_star[a][n][t]
             compa_star = norma_star[a][n][t]
@@ -114,42 +101,22 @@ for a in normd_star:
                 compa_markers = norma_markers[i]
                 diff_d = compd_star-compd_markers
                 diff_a = compa_star-compa_markers
-                print(f'Master: {a}, Normalizer: {n}, Target: {t}, Difference distance: {diff_d}, Difference angle: {diff_a}, Marker: {i}')
-                if abs(diff_d) < 0.5 and abs(diff_a) < 1:
-                    matches[a]['Matches'] += 1
-                    if matches[a]['Matches'] == 1:
-                        matches[a]['Marker 1'] = a
-                        matches[a][f'Marker {list(normd_markers.keys())[0]}'] = n
-                        matches[a][f'Marker {i}'] = t
-                    else:
-                        matches[a][f'Marker {i}'] = t
+                # if a == '26451' and n == '25428' and (t == '21421' or t == '20889' or t == '20894' or t == '20455' or t == '20205' or t == '18724' or t == '15900'):
+                #     print(f'Master: {a}, Normalizer: {n}, Target: {t}, Difference distance: {diff_d}, Difference angle: {diff_a}, Marker: {i}')
+                if abs(diff_d) < 0.1 and abs(diff_a) < 2:
+                    if matches[mid]['Matches'] == 0:
+                        # debug.write(f'Master: {a}, Normalizer: {n}, Target: {t}, Difference distance: {diff_d}, Difference angle: {diff_a}, Marker: {i}\r\n')
+                        matches[mid]['Matches'] += 1
+                        matches[mid]['Marker 1'] = a
+                        matches[mid][f'Marker {list(normd_markers.keys())[0]}'] = n
+                        matches[mid][f'Marker {i}'] = t
+                    elif f'Marker {i}' not in list(matches[mid].keys()):
+                        # debug.write(f'Master: {a}, Normalizer: {n}, Target: {t}, Difference distance: {diff_d}, Difference angle: {diff_a}, Marker: {i}\r\n')
+                        matches[mid]['Matches'] += 1
+                        matches[mid][f'Marker {i}'] = t
 
 for k, v in list(matches.items()):
     if v['Matches'] < len(list(normd_markers))-1:
         del matches[k]
 
 print(matches)
-
-
-# for i in star_distances:
-#     master = i
-#     target1, target2 = list(star_distances[i].keys())[0:2]
-#     print(f'Master: {i}, Target 1: {target1}, Target 2: {target2}')
-#
-#     m1 = 1
-#     m2 = (star_distances[master][target2])/star_distances[master][target1]
-#     # wan_dis = (star_distances[target1][target2])/star_distances[master][target1]
-#     ang = m.radians(star_angles[master][target1]-star_angles[master][target2])
-#     dist = m.sqrt(m1*m1+m2*m2-2*m1*m2*m.cos(ang))
-#     print(dist)
-
-
-# Distance 8886-6686: 4.799
-# Distance 8886-4427: 7.32
-# Angle: 25
-# ?: 3.597
-#
-# Distance 8886-6686: 1
-# Distance 8886-4427: 1.525
-# Angle: 25
-# ?: 0.749
